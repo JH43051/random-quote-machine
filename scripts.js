98956e36130ac7c53dceab60facb7f63f49ff508
John Holler <jh43051@gmail.com>
2019-03-25 15:09:24 -0400

    javascript file cleaned up

--- scripts.js
+++ scripts.js
@@ -1,19 +1,34 @@
-//const projectName = "random-quote-machine";
-//localStorage.setItem('example_project', 'Random Quote Machine');
+////////////////////////////////////////////////////////////////////////////////////////
+// NOTE: This code requires the following libraries to function properyly:            //
+//                                                                                    //
+// jQuery (https://jquery.com/),                                                      //
+// jQuery Color Animation Plugin (https://bitstorm.org/jquery/color-animation/)       //
+////////////////////////////////////////////////////////////////////////////////////////
 
+
+// Global Variables
 let quotesData = null;
+var colors = ['#16a085',
+              '#27ae60',
+              '#2c3e50',
+              '#f39c12',
+              '#e74c3c',
+              '#9b59b6',
+              '#FB6964',
+              '#342224',
+              "#472E32",
+              "#BDBB99",
+              "#77B1A9",
+              "#73A857"];
 
-function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
+// Code to run after window load
+$(document).ready(function() {
+  getQuotes().then(() => getQuote());
 
-var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
-var currentQuote = '', currentAuthor = '';
+  $('#new-quote').on('click', getQuote);
+});
 
-// Opens a new window when sharing quote
-function openURL(url){
-  window.open(url, 'Share', 'width=800, height=600, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
-}
-
-// Grabs list of possible quotes in JSON format via ajax call
+// Grabs list of possible quotes in JSON format via AJAX
 function getQuotes() {
   return $.ajax({
     headers: {
@@ -25,90 +40,62 @@ function getQuotes() {
         quotesData = JSON.parse(jsonQuotes);
       }
     },
-    error: function() {
+     error: function() {
       alert("Sorry, the internet has run out of quotes! Please check back soon.");
     }
   });
 }
 
-function getRandomQuote() {
-  return quotesData.quotes[Math.floor(Math.random() * quotesData.quotes.length)];
-}
-
 function getQuote() {
-
-  let randomQuote = getRandomQuote();
-  
-  currentQuote = randomQuote.quote;
-  currentAuthor = randomQuote.author;
-
-  if(inIframe())
-  {
-    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-
-    $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
-  }
-
-  // Dymnically formats and inserts a citation into the blockquote element
+  // Grabs a random quote object from the JSON data
+  let randomQuote = quotesData.quotes[Math.floor(Math.random() * quotesData.quotes.length)];
+  let currentQuote = randomQuote.quote;
+  let currentAuthor = randomQuote.author;
+  // Inserts references into Twitter/Tumblr link elements
+  $('#tweet-quote').attr('href',
+                          'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+                          encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+  $('#tumblr-quote').attr('href',
+                          'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+                          encodeURIComponent(currentAuthor) +
+                          '&content=' +
+                          encodeURIComponent(currentQuote) +
+                          '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
+  // Inserts a citation into the blockquote element
   $("#text").attr("cite", "https://en.wikipedia.org/wiki/" + currentAuthor.replace(" ", "_"));
-  
+  // Inserts the quote into the blockquote element and animates it
   $("#quote-text").animate(
-    { opacity: 0 },
-    500,
+     { opacity: 0 },
+     500,
     function() {
       $(this).animate({opacity: 1}, 500);
       $('#text').html('<span class="fas fa-quote-left"></span>' + randomQuote.quote);
-    }
+     }
   );
-
+  // Inserts the author into the paragraph element and animates it
   $("#author-text").animate(
-    { opacity: 0 },
-    500,
+     { opacity: 0 },
+     500,
     function() {
       $(this).animate({opacity: 1}, 500);
       $('#author').html("-" + randomQuote.author);
-    }
+     }
   );
-
+  // Picks a random color from our color list
   var color = Math.floor(Math.random() * colors.length);
-
+  // Animates the background color
   $("main").animate(
     {
       backgroundColor: colors[color],
       color: colors[color]
     },
-    1000
+     1000
   );
-
+  // Animates the button colors
   $("button, a").animate(
-    {
-      backgroundColor: colors[color]
+     {
+       backgroundColor: colors[color]
     },
     1000
   );
 }
-
-
-// Runs this code block after html fully loads
-$(document).ready(
-  function() {
-    getQuotes().then(() => {
-      getQuote();
-    });
-    
-
-    $('#new-quote').on('click', getQuote);
-
-    $('#tweet-quote').on('click', function() {
-      if(!inIframe()) {
-        openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-      }
-    });
-
-    $('#tumblr-quote').on('click', function() {
-      if(!inIframe()) {
-        openURL('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
-      }
-    });
-  }
-);
